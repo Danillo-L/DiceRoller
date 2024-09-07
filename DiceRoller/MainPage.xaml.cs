8bd27caad2ff70623672fa436d08e1dfb45fae65
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-09-06 22:39:02 -0300

    Aprendemos a pegar o número do slider e exibir

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -11,23 +11,26 @@ public partial class MainPage : ContentPage
 
     Random random = new Random();
 
-    public void Random(int x, int y)
+
+    private void RollButton_Clicked(object sender, EventArgs e)
+    {
+        int numeroSelecionado = Convert.ToInt32(SidesPicker.SelectedItem);
+        int numeroSorteado = new Random().Next(1, numeroSelecionado + 1);
+        ResultLabel.Text = numeroSorteado.ToString();
+    }
 
 
 
- 
+    // Primeira etapa
+    // Sortear um número aleatório através do clique do botão
 
+    // Conseguir selecionar a quantidade de lados a partir do Picker
+    // 1 equivale ao menor número que o dado é capaz de sortear
+    // Numero selecionado é o valor de lados que o dado tem
+    // Devolver o número sorteado para a interface
+    // O maior numero sorteavel pelo dado é dado por Numero Sorteado + 1
 
-
-
-
-
-
-
-
-    //Sortear um número aleatório através do clique do botão
-    //Devolver o número sorteado para a interface
-    //Conseguir selecionar a quantidade de lados a partir do Picker
+    // Devolver o número sorteado para a interface
 
 }
 
